d6f876b72dfdc97f0679d4ad3afdaf5bf37ffe05
Muhammad Banhawy <muhammadbanhawy@gmail.com>
2023-03-13 19:15:05 +0200

    IProblemDAO Updated

--- server/datastore/dao/ProblemDao.ts
+++ server/datastore/dao/ProblemDao.ts
@@ -1,10 +1,10 @@
 import { IProblem, ITag } from "@bugzone/shared/types";
 export interface IProblemDAO {
-  create(problem: IProblem): Promise<IProblem>;
-  getOne(id: string): Promise<IProblem | undefined | null>;
-  getAll(): Promise<IProblem[]>;
-  getByTags(tags: ITag[]): Promise<IProblem[]>;
-  getByUser(userId: string): Promise<IProblem[]>;
-  update(problem: Partial<IProblem>): Promise<IProblem>;
-  delete(id: string): Promise<IProblem>;
+  createProblem(problem: IProblem): Promise<IProblem>;
+  getProblem(id: string): Promise<IProblem | undefined | null>;
+  getAllProblems(): Promise<IProblem[] | undefined>;
+  getProblemsByTags(tags: ITag[]): Promise<IProblem[]>;
+  getProblemsByUser(userId: string): Promise<IProblem[]>;
+  updateProblem(problem: Partial<IProblem>, userId?: string): Promise<IProblem>;
+  deleteProblem(id: string, userId?: string): Promise<IProblem>;
 }
