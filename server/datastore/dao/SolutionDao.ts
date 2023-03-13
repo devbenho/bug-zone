50cd9b3ca643f92fb0de93f3abadb2e9ffa7ada5
Muhammad Banhawy <muhammadbanhawy@gmail.com>
2023-03-13 22:19:00 +0200

    ISolutionDAO Created

--- server/datastore/dao/SolutionDao.ts
+++ server/datastore/dao/SolutionDao.ts
@@ -1,1 +1,12 @@
-export interface ISolutionDAO {}
+import { ISolution, ITag } from "@bugzone/shared/types";
+
+export interface ISolutionDAO {
+  createSolution(solution: ISolution): Promise<void>;
+  getSolution(id: string): Promise<ISolution | undefined | null>;
+  getAllSolutions(): Promise<ISolution[]>;
+  getProblemSolutions(problemId: string): Promise<ISolution[]>;
+  getSolutionsByTags(tags: ITag[]): Promise<ISolution[]>;
+  getSolutionsByUser(userId: string): Promise<ISolution[]>;
+  updateSolution(solution: Partial<ISolution>): Promise<ISolution>;
+  deleteSolution(id: string): Promise<ISolution>;
+}
