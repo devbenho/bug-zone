cd6864dabf9eeac76ea4f4de09e110ae7adea388
Muhammad Banhawy <muhammadbanhawy@gmail.com>
2023-03-13 22:47:28 +0200

    IReactDAO Created

--- server/datastore/dao/ReactDao.ts
+++ server/datastore/dao/ReactDao.ts
@@ -1,1 +1,16 @@
-export interface IReactDAO {}
+import { IReact } from "@bugzone/shared/types";
+
+export interface IReactDAO {
+  createReact(react: IReact): Promise<void>;
+  deleteReact(react: IReact): Promise<void>;
+  getAllReacts(
+    contentType: "problem" | "solution",
+    contentId: string
+  ): Promise<IReact[]>;
+  countReacts(
+    contentType: "problem" | "solution",
+    contentId: string
+  ): Promise<{ likes: number; dislikes: number }>;
+  getUserReacts(userId: string): Promise<IReact[]>;
+  isReacted(react: IReact): Promise<boolean>;
+}
