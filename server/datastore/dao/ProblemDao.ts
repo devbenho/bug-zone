1e34b38aed60030b54d2eab5a2b7eae4f1399947
Muhammad Banhawy <muhammadbanhawy@gmail.com>
2023-03-14 14:09:20 +0200

    In Memory Database finished

--- server/datastore/dao/ProblemDao.ts
+++ server/datastore/dao/ProblemDao.ts
@@ -2,11 +2,10 @@ import { IProblem, ITag } from "@bugzone/shared/types";
 export interface IProblemDAO {
   createProblem(problem: IProblem): Promise<IProblem>;
   getProblem(id: string): Promise<IProblem | undefined | null>;
-  getAllProblems(): Promise<IProblem[] | undefined>;
-  //Get all problems of @devbenho
-  getAllProblems(userId: string): Promise<IProblem[] | undefined>;
-  //Get all problems with these tags['web' - 'api' - rest]
-  getAllProblems(tags: ITag[]): Promise<IProblem[] | undefined>;
+  getAllProblems(
+    tags?: ITag[],
+    userId?: string
+  ): Promise<IProblem[] | undefined>;
   updateProblem(problem: Partial<IProblem>): Promise<IProblem>;
   deleteProblem(id: string): Promise<IProblem>;
 }
