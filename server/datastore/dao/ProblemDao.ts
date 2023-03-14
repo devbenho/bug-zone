88a351915fb3fe4cf0a7fc93bc7ca442b1d1d7c6
Muhammad Banhawy <muhammadbanhawy@gmail.com>
2023-03-14 13:19:21 +0200

    IProblemDAO Modified

--- server/datastore/dao/ProblemDao.ts
+++ server/datastore/dao/ProblemDao.ts
@@ -3,8 +3,10 @@ export interface IProblemDAO {
   createProblem(problem: IProblem): Promise<IProblem>;
   getProblem(id: string): Promise<IProblem | undefined | null>;
   getAllProblems(): Promise<IProblem[] | undefined>;
-  getProblemsByTags(tags: ITag[]): Promise<IProblem[]>;
-  getProblemsByUser(userId: string): Promise<IProblem[]>;
+  //Get all problems of @devbenho
+  getAllProblems(userId: string): Promise<IProblem[] | undefined>;
+  //Get all problems with these tags['web' - 'api' - rest]
+  getAllProblems(tags: ITag[]): Promise<IProblem[] | undefined>;
   updateProblem(problem: Partial<IProblem>): Promise<IProblem>;
   deleteProblem(id: string): Promise<IProblem>;
 }
