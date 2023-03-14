2e5f378fc7f949f317e27a401b6cb1196c6cb73b
Muhammad Banhawy <muhammadbanhawy@gmail.com>
2023-03-14 13:20:53 +0200

    IDataStore extends ITagDAO

--- server/datastore/index.ts
+++ server/datastore/index.ts
@@ -3,10 +3,12 @@ import { IProblemDAO } from "./dao/ProblemDao";
 import { ISolutionDAO } from "./dao/SolutionDao";
 import { IReactDAO } from "./dao/ReactDao";
 import { InMemoryDB } from "./memorydb";
+import { ITagDAO } from "./dao/TagDao";
 export interface IDataStore
   extends IUserDAO,
     IProblemDAO,
     ISolutionDAO,
-    IReactDAO {}
+    IReactDAO,
+    ITagDAO {}
 
 export const db = new InMemoryDB();
