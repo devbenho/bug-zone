46dee5148df45473006de74b0a1fb6862cad34f0
devBenho <devbenho@gmail.com>
2023-05-03 12:09:57 +0300

    User model v1 created (auth-demo)

--- server/models/user.model.ts
+++ server/models/user.model.ts
@@ -1,15 +1,40 @@
-import { IUser } from '@bugzone/shared/types';
-import { Db, MongoClient, Collection, ObjectId } from 'mongodb';
+import { Model, Schema, model } from 'mongoose'
+import bcrypt from 'bcrypt'
 
-export class UserModel {
-  private collection: Collection<IUser>;
-
-  constructor(db: Db) {
-    this.collection = db.collection<IUser>('users');
-  }
-
-  async findById(id: string): Promise<IUser | null> {
-    const user = await this.collection.findOne({ _id: new ObjectId(id) });
-    return user;
-  }
+interface IUser {
+  username: string
+  email: string
+  password: string
 }
+
+// Put all user instance methods in this interface:
+interface IUserMethods {
+  passwordMatch(pw: string): boolean
+}
+
+// Create a new Model type that knows about IUserMethods...
+type UserModel = Model<IUser, {}, IUserMethods>
+
+// And a schema that knows about IUserMethods
+const schema = new Schema<IUser, UserModel, IUserMethods>({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+})
+
+schema.pre('save', async function (next) {
+  const user = this
+
+  if (!user.isModified('password')) return next()
+
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(user.password, salt)
+  user.password = hashedPassword
+  next()
+})
+
+schema.method('passwordMatch', function passwordMatch(pw: string) {
+  return bcrypt.compareSync(pw, this.password)
+})
+
+export const User = model<IUser, UserModel>('User', schema)
