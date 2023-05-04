b8e95db25a6571edafc34e7f9af8b7e2d1c496dc
devBenho <devbenho@gmail.com>
2023-05-04 09:12:38 +0300

    auth handler v1

--- server/handlers/auth.handler.ts
+++ server/handlers/auth.handler.ts
@@ -1,53 +1,56 @@
-import { SignInRequest, SignUpRequest, SignUpResponse } from '@bugzone/shared/apiTypes'
-import { db } from '../datastore'
-import { IUser } from '@bugzone/shared/types'
-import { CustomReqResHandler } from '../types'
-import { generateJWT, hashPassword } from '../utils/auth'
-import { validateInput } from '../utils/validator'
+import { Request, Response, NextFunction } from 'express'
+import { User } from '../models/user.model'
+import { AuthService } from '../services/auth.service'
 
-export const signUpHandler: CustomReqResHandler<SignUpRequest, SignUpResponse> = async (
-  req,
-  res,
-  next
-) => {
-  const { username, email, password, bio, socialLinks, interests, profilePic } = req.body
-  validateInput<SignUpRequest, { username: string; email: string; password: string }>(req.body, {
-    username,
-    email,
-    password,
-  })
-  const user: IUser = {
-    id: crypto.randomUUID(),
-    username,
-    email,
-    password: hashPassword(password),
-    bio,
-    socialLinks,
-    interests,
-    profilePic,
-    activity: {
-      problems: [],
-      solutions: [],
-      reacts: [],
-    },
-    createdAt: new Date(),
+export const register = async (req: Request, res: Response, _next: NextFunction) => {
+  // check fields
+  const { username, email, password } = req.body
+  if (!username || !email || !password) {
+    return res.status(400).send({
+      success: false,
+      messsage: 'All fields are required',
+    })
   }
-  // Store the user in DB and return ID
-  const newUser = await db.createUser(user)
-  // Generate JWT for the user using his ID
-  const jwt = generateJWT({ userId: newUser! })
-  res.send({ success: true, message: `${username} created`, jwt })
-  console.log(user)
-  next()
+  // is user exist
+  const user = await User.findOne({ username })
+  if (user) {
+    return res.status(403).send({
+      success: false,
+      message: 'This user is already exist',
+    })
+  }
+  // create user
+  const newUser = await User.create({ username, email, password })
+  //   Generate Token
+  const jwt: string = await new AuthService().signToken({ userId: newUser._id.toHexString() })
+  // response
+  return res.status(200).send({
+    success: true,
+    jwt,
+  })
 }
 
-export const signInHandler: CustomReqResHandler<SignInRequest, SignUpResponse> = async (
-  req,
-  res,
-  _next
-) => {
+export const login = async (req: Request, res: Response, _next: NextFunction) => {
+  // check fields
   const { login, password } = req.body
   if (!login || !password) {
-    return res.sendStatus(400)
+    return res.status(400).send({
+      success: false,
+      messsage: 'All fields are required',
+    })
   }
+  // lw user msh mwgood
+  const user = await User.findOne({ $or: [{ username: login }, { email: login }] })
+  if (!user || !user.passwordMatch(password)) {
+    return res.status(403).send({
+      success: false,
+      message: 'Invalid Data',
+    })
+  }
+  //   Generate Token
+  const token = await new AuthService().signToken({ userId: user._id.toHexString() })
+
+  res.cookie('jwt', token, { httpOnly: true, sameSite: true })
+
+  return res.status(200).json({ success: true, token })
 }
