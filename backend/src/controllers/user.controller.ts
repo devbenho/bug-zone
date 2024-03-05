7fc48b7120df5d3d0ab9dc3285e2142e6236e926
devBenho <devbenho@gmail.com>
2024-03-05 14:38:57 +0200

    TODO: Apply pagination & filter & sort

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -7,10 +7,8 @@ import BaseController from "./base.controller";
 @injectable()
 export class UserController extends BaseController {
   @inject("IUserService") private _userService: IUserService;
-
-  public async getAll(req: Request, res: Response) {
+  public async getAll(_req: Request, res: Response) {
     const result = await this._userService.getAll();
-    // apply pagination, filtering, sorting, etc.
 
     return res.status(200).json({
       length: result?.length,
