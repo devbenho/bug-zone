b651a1a942acdc85afc1e52dd17c7cfdecbc5c00
devBenho <devbenho@gmail.com>
2024-03-05 14:43:46 +0200

    IMPORT: Register DTOs imported

--- backend/src/services/auth/auth.service.ts
+++ backend/src/services/auth/auth.service.ts
@@ -1,9 +1,13 @@
 import { LoginRequestDto } from "../../dtos/requests/auth/login.dto";
+import { RegisterUserRequestDto } from "../../dtos/requests/auth/register.dto";
 import { LoginResponseDto } from "../../dtos/responses/auth/login.dto";
+import { RegisterUserResponseDto } from "../../dtos/responses/auth/register.dto";
 
 interface IAuthService {
   login: (payload: LoginRequestDto) => Promise<LoginResponseDto>;
-  register: (email: string, password: string) => Promise<string>;
+  register: (
+    payload: RegisterUserRequestDto,
+  ) => Promise<RegisterUserResponseDto>;
   logout: (token: string) => Promise<void>;
   verify: (token: string) => Promise<void>;
   refreshToken: (token: string) => Promise<string>;
