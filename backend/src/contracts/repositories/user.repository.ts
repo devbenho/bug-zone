e364eb1bf6ee0545214f1024f1ab10ce525a6229
devBenho <devbenho@gmail.com>
2024-04-09 20:22:06 +0200

    UPDATE: IUserRepository Updated

--- backend/src/contracts/repositories/user.repository.ts
+++ backend/src/contracts/repositories/user.repository.ts
@@ -1,19 +1,13 @@
-import User from '../../web/rest/database/entities/user.entity';
-import { CreateUserRequestDto } from '../../web/rest/dtos/requests/user/create.dto';
-import { DeleteUserRequestDto } from '../../web/rest/dtos/requests/user/delete.dto';
-import { GetUserByEmailRequestDto } from '../../web/rest/dtos/requests/user/get-by-email.dto';
-import { GetUserByIdRequestDto } from '../../web/rest/dtos/requests/user/get-by-id.dto';
-import { GetUserByUsernameRequestDto } from '../../web/rest/dtos/requests/user/get-by-username.dto';
-import { UpdateUserRequestDto } from '../../web/rest/dtos/requests/user/update.dto';
+import { User } from '@domain/entities';
 
 export interface IUserRepository {
-  create(user: CreateUserRequestDto): Promise<User>;
-  findById(id: GetUserByIdRequestDto): Promise<User | null>;
-  findByEmail(email: GetUserByEmailRequestDto): Promise<User | null>;
-  update(payload: UpdateUserRequestDto): Promise<User>;
-  delete(paload: DeleteUserRequestDto): Promise<boolean>; // soft delete
+  create(user: User): Promise<User>;
+  findById(id: string): Promise<User | null>;
+  findByEmail(email: string): Promise<User | null>;
+  update(user: User): Promise<User>;
+  delete(user: User): Promise<boolean>; // soft delete
   // hardDelete(id: string): Promise<boolean>;
-  findByUsername(payload: GetUserByUsernameRequestDto): Promise<User | null>;
-  isEmailExists(payload: GetUserByEmailRequestDto): Promise<boolean>;
-  isUsernameExists(payload: GetUserByUsernameRequestDto): Promise<boolean>;
+  findByUsername(payload: string): Promise<User | null>;
+  isEmailExists(payload: string): Promise<boolean>;
+  isUsernameExists(payload: string): Promise<boolean>;
 }
