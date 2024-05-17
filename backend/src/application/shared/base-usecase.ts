da2ee8381a2039d2150f035686ede9ee93d7d029
Muhammad Banhawy <devbenho@gmail.com>
2024-05-18 00:05:45 +0300

    Adding base-use-case @application/shared

--- backend/src/application/shared/base-usecase.ts
+++ backend/src/application/shared/base-usecase.ts
@@ -1,34 +1,22 @@
 import { performance } from 'node:perf_hooks';
 
 import { UseCaseRequest } from './usecase.request';
-import { Logger } from '@domain/logger';
+import { injectable } from 'inversify';
 
+@injectable()
 abstract class BaseUseCase<IRequest extends UseCaseRequest, IResponse> {
   public async execute(request: IRequest): Promise<IResponse> {
     try {
       const startTime = performance.now();
-
       request.validate();
-
       const response = await this.performOperation(request);
-
       const endTime = performance.now();
-
       const useCaseExecutionTime = endTime - startTime;
-
-      Logger.info(
-        `${this.constructor.name}.execute(${request}) took +${useCaseExecutionTime} ms to execute!`,
-      );
-
       return response;
     } catch (error) {
-      Logger.error(
-        `[@UseCase] ${this.constructor.name}.execute(${request}) threw the following error! --- ${error}`,
-      );
       throw error;
     }
   }
-
   protected abstract performOperation(request: IRequest): Promise<IResponse>;
 }
 
