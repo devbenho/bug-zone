0a9403e227134c3675c4d20a85b38202a98bf7bd
Muhammad Banhawy <devbenho@gmail.com>
2024-05-18 00:09:25 +0300

    Adding  friendship and reply persistence entities @infrastructure layer

---
+++ backend/src/infrastructure/shared/persistence/entities/friendship.entity.ts
@@ -0,0 +1,14 @@
+import { Column, Entity } from 'typeorm';
+import BaseEntity from './base.entity';
+
+@Entity()
+export class Friendship extends BaseEntity {
+  @Column()
+  userId: string;
+
+  @Column()
+  friendId: string;
+
+  @Column()
+  type: 'pending' | 'accepted' | 'rejected' | 'blocked';
+}
