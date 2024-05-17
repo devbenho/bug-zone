9b59c670e1e1135b00aee06c49c0ef6e61198838
Muhammad Banhawy <devbenho@gmail.com>
2024-05-17 23:05:35 +0300

    Removing old DI logic at presentation layer

--- backend/src/infrastructure/shared/config/dependency-injection.ts
+++
@@ -1,8 +0,0 @@
-import { Container } from 'inversify';
-import { ILocalizer } from '@contracts/services/ILocaizer';
-import { Localizer } from '../services/Localizer';
-
-const container = new Container();
-container.bind<ILocalizer>(ILocalizer).to(Localizer);
-
-export default container;
