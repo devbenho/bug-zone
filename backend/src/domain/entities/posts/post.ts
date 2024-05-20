6aa9a0437c58cdbcd437340177800c6231d8e533
Muhammad Banhawy <devbenho@gmail.com>
2024-05-20 10:03:09 +0300

    Modifying Post entity (refactor the fields and adding factory pattern to create the Obj)

--- backend/src/domain/entities/posts/post.ts
+++ backend/src/domain/entities/posts/post.ts
@@ -1,18 +1,38 @@
-import likePost from '@infrastructure/LikePosts/like-post.persistence';
-import { Comment } from '@domain/entities/comment';
-import { User } from '@domain/entities/user';
 import { AuditableBaseEntity } from '@domain/shared/auditable.entity';
+import { User } from '../user';
+import { LikePost } from '../like-post';
+import { Comment } from '../comment';
+import { Nullable } from '@domain/types';
 
 class Post extends AuditableBaseEntity {
+  public author: User;
+  public comments: Nullable<Promise<Comment[]>>;
+  public likes: Nullable<Promise<LikePost[]>>;
+  public status: string;
+
   constructor(
     public title: string,
     public content: string,
     public authorId: string,
-    public author: User,
-    public comments: Comment[],
-    public likes: likePost[],
+    author: User,
+    comments: Comment[],
+    likes: LikePost[],
   ) {
     super(new Date(), authorId, new Date(), authorId, null, null);
+    this.author = author;
+    this.comments = comments.length ? Promise.resolve(comments) : null;
+    this.likes = likes.length ? Promise.resolve(likes) : null;
+    this.status = 'active';
+  }
+
+  public static create(
+    title: string,
+    content: string,
+    authorId: string,
+    author: User,
+  ): Post {
+    return new Post(title, content, authorId, author, [], []);
   }
 }
+
 export { Post };
