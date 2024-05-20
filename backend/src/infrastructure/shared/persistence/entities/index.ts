6f65e54a0fddd466b2d884089d261f8f23cf22b0
Muhammad Banhawy <devbenho@gmail.com>
2024-05-20 10:20:00 +0300

    refactoring shared entities module @infra

--- backend/src/infrastructure/shared/persistence/entities/index.ts
+++ backend/src/infrastructure/shared/persistence/entities/index.ts
@@ -1,7 +1,1 @@
-export * from '../../../comments/comment.persistence';
-export * from './friendship.entity';
-export * from './like-comment.entity';
-export * from '../../../posts/post.persistence';
-export * from './reply.entity';
-export * from '../../../users/user.persistence';
 export * from './base.entity';
