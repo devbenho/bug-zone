1a1c930a2dfe220d01f737c3a1948a9dda4106a2
Muhammad Banhawy <devbenho@gmail.com>
2024-05-20 09:56:15 +0300

    User infrastructure featue has been implemented including (IRepository, Repository.imp, user.persistence and user mapper)

--- backend/src/domain/repositories/user.repository.ts
+++ backend/src/domain/repositories/user.repository.ts
@@ -1,13 +1,13 @@
 import { User } from '@domain/entities';
 
 export interface IUserRepository {
-  create(user: User): Promise<User>;
+  saveUser(user: User): Promise<User>;
   findById(id: string): Promise<User | null>;
   findByEmail(email: string): Promise<User | null>;
-  update(user: User): Promise<User>;
   delete(user: User): Promise<boolean>; // soft delete
   // hardDelete(id: string): Promise<boolean>;
   findByUsername(payload: string): Promise<User | null>;
   isEmailExists(payload: string): Promise<boolean>;
   isUsernameExists(payload: string): Promise<boolean>;
+  updateUser(user: User): Promise<User>;
 }
--- backend/src/infrastructure/users/user.persistence.ts
+++ backend/src/infrastructure/users/user.persistence.ts
@@ -1,14 +1,25 @@
 import { Role } from '@domain/eums/role.enum';
-import { Entity, Column, OneToMany, Unique, BeforeInsert } from 'typeorm';
-import LikePostPersistence from '../LikePosts/like-post.persistence';
+import {
+  Entity,
+  Column,
+  OneToMany,
+  Unique,
+  BeforeInsert,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm';
 import { Reply } from '@domain/entities';
 import CommentPersistence from '../comments/comment.persistence';
 import { PostPersistence } from '../posts/post.persistence';
-import { BaseEntity } from '@domain/shared/base.entity';
+import { LikeCommentPersistence } from '@infrastructure/like-comments/like-comment.persistence';
+import LikePostPersistence from '@infrastructure/like-posts/like-post.persistence';
+import BaseEntity from '@infrastructure/shared/persistence/entities/base.persistence';
+import { Nullable } from '@domain/types';
 
-
-@Entity()
-class UserPersistence extends BaseEntity {
+@Entity('users')
+class UserPersistence {
   constructor(
     firstName: string,
     lastName: string,
@@ -19,7 +30,6 @@ class UserPersistence extends BaseEntity {
     role: Role = Role.USER,
     profilePicture?: string,
   ) {
-    super();
     this.firstName = firstName;
     this.lastName = lastName;
     this.username = username;
@@ -29,6 +39,19 @@ class UserPersistence extends BaseEntity {
     this.role = role;
     this.profilePicture = profilePicture;
   }
+
+  @PrimaryGeneratedColumn('uuid')
+  id: Nullable<string>;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn({ nullable: true })
+  deletedAt: Nullable<Date>;
+
   @Column()
   firstName: string;
 
@@ -53,20 +76,28 @@ class UserPersistence extends BaseEntity {
   @Column({ nullable: false, default: 'user' })
   role: Role;
 
-  @OneToMany(() => LikePostPersistence, likePost => likePost.user)
-  likedPosts: LikePostPersistence[];
+  @OneToMany(() => LikePostPersistence, likePost => likePost.user, {
+    lazy: true,
+  })
+  likedPosts: Promise<LikePostPersistence[]>;
 
-  @OneToMany(() => Reply, reply => reply.userId)
+  @OneToMany(() => Reply, reply => reply.userId, { lazy: true })
   replies: Reply[];
 
-  @OneToMany(() => CommentPersistence, comment => comment.user)
+  @OneToMany(() => CommentPersistence, comment => comment.user, { lazy: true })
   comments: CommentPersistence[];
 
-  @OneToMany(() => PostPersistence, post => post.author)
+  @OneToMany(() => PostPersistence, post => post.author, { lazy: true })
   posts: PostPersistence[];
 
-  @OneToMany(() => PostPersistence, post => post.author)
+  @OneToMany(() => PostPersistence, post => post.author, { lazy: true })
   editedPosts: PostPersistence[];
+
+  // add likeComment
+  @OneToMany(() => LikeCommentPersistence, likeComment => likeComment.user, {
+    lazy: true,
+  })
+  likedComments: LikeCommentPersistence[];
 
   @Column({ nullable: true })
   profilePicture?: string;
