1605046715301f90061a2db88cd3c99653d5ee8d
Muhammad Banhawy <devbenho@gmail.com>
2024-05-24 07:31:29 +0300

    Adding entities and subscribers to load (typeorm config)

--- server/src/infrastructure/shared/persistence/data-source.ts
+++ server/src/infrastructure/shared/persistence/data-source.ts
@@ -1,14 +1,69 @@
-import { DataSource } from 'typeorm';
+import 'reflect-metadata';
+import {
+  CreateDateColumn,
+  DataSource,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+// import { UserSubscriber } from './subscribers/user.subscribe';
+import path from 'path';
+import { Nullable } from '@domain/shared/types';
+import { UserPersistence } from '@infrastructure/users';
+import { LikePostPersistence } from '@infrastructure/like-posts';
+import { LikeReplyPersistence } from '@infrastructure/like-replies';
+import { PostPersistence } from '@infrastructure/posts';
+import { CommentPersistence } from '@infrastructure/comments';
+import { ReplyPersistence } from '@infrastructure/replies';
+import { LikeCommentPersistence } from '@infrastructure/like-comments';
 import { UserSubscriber } from './subscribers/user.subscribe';
 
+// generate test typeorm entity called DemoEntity
+//
+@Entity()
+class BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @DeleteDateColumn()
+  deletedAt: Nullable<Date>;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  constructor(
+    id: string,
+    deletedAt: Nullable<Date>,
+    createdAt: Date,
+    updatedAt: Date,
+  ) {
+    this.id = id;
+    this.deletedAt = deletedAt;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+  }
+}
 const appDataSource = new DataSource({
   type: 'sqlite',
   database: 'db.sqlite3',
   synchronize: true,
   logging: true,
-  entities: [__dirname + '/../**/*.persistence.ts'],
-  migrations: [__dirname + '/../migrations/**/*.ts'],
+  entities: [
+    UserPersistence,
+    LikePostPersistence,
+    LikeReplyPersistence,
+    PostPersistence,
+    CommentPersistence,
+    ReplyPersistence,
+    LikeCommentPersistence,
+    LikeCommentPersistence,
+  ],
+  migrations: [path.join(__dirname, '/../migrations/**/*.{js,ts}')],
   subscribers: [UserSubscriber],
 });
 
-export default appDataSource;
+export { appDataSource };
