cefe83aaef5938663243301396eb4983610c4764
Muhammad Banhawy <devbenho@gmail.com>
2024-05-24 07:32:01 +0300

    DB Init using Typeorm dataStore

--- server/src/web/rest/app.ts
+++ server/src/web/rest/app.ts
@@ -6,16 +6,45 @@ import { errorHandlerMiddleware } from './middlewares/error.mw';
 import { LOGGER } from './logger';
 import ApplicationRouter from './routes';
 import { container } from '@infrastructure/shared/ioc/inversify.config';
+import { appDataSource } from '@infrastructure/shared/persistence/data-source';
+import { UserMapper, UserPersistence } from '@infrastructure/users';
+import { Role } from '@domain/eums/role.enum';
+import { User } from '@domain/entities';
+import { AuditableBaseEntity } from '@domain/shared/auditable.entity';
+import { IMapper } from '@application/shared';
+import { CreateUserDto } from '@contracts/dtos/users';
+import { TriggeredBy, TriggeredByUser } from '@domain/shared/entities';
 const app = express();
 dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+appDataSource
+  .initialize()
+  .then(() => {
+    // LOGGER.info(appDataSource.subscribers);
+    // log the loaded entities
+    LOGGER.info('Loaded entities');
+    LOGGER.info(appDataSource.entityMetadatas.length);
+    LOGGER.info(appDataSource.subscribers.length);
+    LOGGER.info('Data source initialized');
+  })
+  .catch(err => {
+    LOGGER.error(err);
+  });
 app.use(morgan('dev'));
 const PORT = process.env.PORT || 3004;
 // Check ENDPOINT;
-app.get('/healthZ', (_req, res) => {
-  return res.status(200).json({ message: 'OK' });
+app.get('/healthz', async (_req, res) => {
+  try {
+    return res.status(200).json({
+      message: 'Server is running',
+    });
+  } catch (error) {
+    LOGGER.error('Error occurred:', error);
+    return res.status(500).json({
+      message: 'Internal Server Error',
+    });
+  }
 });
 // validate the configs
 validateConfigs();
