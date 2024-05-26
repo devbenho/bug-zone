3ed314b8f89dff3a6998d75a610f8c84eedffd22
Muhammad Banhawy <devbenho@gmail.com>
2024-05-27 01:25:43 +0300

    Adding post bindings

--- server/src/infrastructure/shared/ioc/types.ts
+++ server/src/infrastructure/shared/ioc/types.ts
@@ -4,12 +4,17 @@ let TYPES = {
   IHasherService: Symbol.for('IHasherService'),
   ILoginInputPort: Symbol.for('ILoginInputPort'),
   IRegisterInputPort: Symbol.for('IRegisterInputPort'),
+
+  ICreatePostInputPort: Symbol.for('ICreatePostInputPort'),
+  ICreatePostOutputPort: Symbol.for('ICreatePostOutputPort'),
+
   ILoginOutputPort: Symbol.for('ILoginOutputPort'),
   IRegisterOutputPort: Symbol.for('IRegisterOutputPort'),
   IUserRepository: Symbol.for('IUserRepository'),
   IPostRepository: Symbol.for('IPostRepository'),
   IUserController: Symbol.for('IUserController'),
   IAuthController: Symbol.for('IAuthController'),
+  IPostController: Symbol.for('IPostController'),
   ApplicationRouter: Symbol.for('ApplicationRouter'),
   DataSource: Symbol.for('DataSource'),
   IUserMapper: Symbol.for('IMapper<User, CreateUserDto>'),
