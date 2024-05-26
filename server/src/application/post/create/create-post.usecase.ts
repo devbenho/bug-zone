d53c4c3df9d1dee9ba78ec6f60f19102f4010bdc
Muhammad Banhawy <devbenho@gmail.com>
2024-05-27 01:17:01 +0300

    Adding create post use-case

--- server/src/application/post/create/create-post.usecase.ts
+++ server/src/application/post/create/create-post.usecase.ts
@@ -1,20 +1,32 @@
 import { BaseUseCase, UseCase } from '@application/shared';
 import { CreatePostRequest } from './create-post.request';
 import { PostDetailsResponseDto } from '@contracts/dtos/posts';
+import { inject, injectable } from 'inversify';
+import { TYPES } from '@infrastructure/shared/ioc/types';
+import { IPostRepository } from '@domain/repositories/post.repository';
+import { LOGGER } from '@/web/rest/logger';
 
-@UseCase()
+@injectable()
 class CreatePostUseCase extends BaseUseCase<
   CreatePostRequest,
   PostDetailsResponseDto
 > {
-  constructor() {
+  constructor(
+    @inject(TYPES.IPostRepository) private _postRepository: IPostRepository,
+  ) {
     super();
   }
 
-  public async performOperation({}: CreatePostRequest): Promise<PostDetailsResponseDto> {
-    throw new Error('Method not implemented.');
+  public async performOperation(
+    request: CreatePostRequest,
+  ): Promise<PostDetailsResponseDto> {
+    const post = CreatePostRequest.toEntity(request);
+    const createdPost = await this._postRepository.createPost(post);
+    if (createdPost) {
+      return PostDetailsResponseDto.fromEntity(createdPost);
+    }
+    return {} as PostDetailsResponseDto;
   }
 }
 
 export { CreatePostUseCase };
-
