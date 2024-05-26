711f10db893e4149cdafed0db23b2f62cabaafca
Muhammad Banhawy <devbenho@gmail.com>
2024-05-27 01:19:23 +0300

    Adding post dependencies in the IOC container

--- server/src/infrastructure/shared/ioc/inversify.config.ts
+++ server/src/infrastructure/shared/ioc/inversify.config.ts
@@ -11,6 +11,9 @@ import { LoginUseCase } from '@application/auth/login/login.use-case';
 import { RegisterUsecase } from '@application/auth/register/register.use-case';
 import { DataSource } from 'typeorm';
 import { appDataSource } from '@infrastructure/shared/persistence/data-source';
+import { PostsController } from '@/web/rest/controllers/posts.controller';
+import { PostRepository } from '@infrastructure/posts';
+import { CreatePostUseCase } from '@application/post';
 
 const container = new Container();
 // Bind the extrernal dependencies
@@ -20,13 +23,17 @@ container.bind(TYPES.IUserMapper).to(UserMapper);
 
 // Inject the repositories
 container.bind(TYPES.IUserRepository).to(UserRepository);
+container.bind(TYPES.IPostRepository).to(PostRepository);
 
 // Inject input ports
 container.bind(TYPES.ILoginInputPort).to(LoginUseCase);
 container.bind(TYPES.IRegisterInputPort).to(RegisterUsecase);
 
+container.bind(TYPES.ICreatePostInputPort).to(CreatePostUseCase);
+
 // Bind the controllers
 container.bind(TYPES.IAuthController).to(AuthController);
+container.bind(TYPES.IPostController).to(PostsController);
 
 // Bind ApplicationRouter
 container.bind<ApplicationRouter>(ApplicationRouter).to(ApplicationRouter);
