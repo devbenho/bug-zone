98032ff378704936eed1a446153872d34358f81a
Muhammad Banhawy <devbenho@gmail.com>
2024-06-02 12:14:39 +0300

    Adding index file for exceptions and middleware

---
+++ server/src/web/rest/middlewares/index.ts
@@ -0,0 +1,5 @@
+export * from './error-handler.middleware';
+export * from './error.mw';
+export * from './login.mw';
+export * from './not-found.middleware';
+export * from './metadata.middleware';
