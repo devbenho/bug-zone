6dd99e2fd6b460502310cfab400b2e4fdb3527d2
Muhammad Banhawy <devbenho@gmail.com>
2024-06-02 12:11:41 +0300

    Login use-case cleanup

--- server/src/application/auth/login/login.use-case.ts
+++ server/src/application/auth/login/login.use-case.ts
@@ -1,12 +1,12 @@
 import { AuthRequest, AuthResponseDto } from '@contracts/dtos/auth';
 import { UnauthorizedError } from '@contracts/errors/unauthorized.error';
 import { IHasherService } from '@contracts/services/IHasher';
-import { IJwtService, JwtPayload } from '@contracts/services/IJwt';
+import { IJwtService } from '@contracts/services/IJwt';
 import { TYPES } from '@infrastructure/shared/ioc/types';
 import { inject, injectable } from 'inversify';
 import { BaseUseCase } from '@application/shared';
 import { IUserRepository } from '@domain/repositories/user.repository';
-import { log } from 'console';
+
 @injectable()
 class LoginUseCase extends BaseUseCase<AuthRequest, AuthResponseDto> {
   constructor(
@@ -16,7 +16,9 @@ class LoginUseCase extends BaseUseCase<AuthRequest, AuthResponseDto> {
   ) {
     super();
   }
-  public async performOperation(request: AuthRequest): Promise<AuthResponseDto> {
+  public async performOperation(
+    request: AuthRequest,
+  ): Promise<AuthResponseDto> {
     const user =
       (await this._userRepository.findByEmail(request.login)) ||
       (await this._userRepository.findByUsername(request.login));
